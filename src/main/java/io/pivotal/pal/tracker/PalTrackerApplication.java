a1ece5a1ec8c943411372ff80702292610694da3
Ubuntu <ubuntu@ip-172-31-91-197.ec2.internal>
2021-04-22 18:16:08 +0000

    spring jdbcTemplate

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,18 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
-    public TimeEntryRepository getTimeEntryRepo() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository getTimeEntryRepo(DataSource dataSource)
+    {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
